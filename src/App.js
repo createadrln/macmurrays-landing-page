b79a966c5bd36d548293ccdd39227fbf548f85cd
Ryan MacMurray <macmurray_ryan@bah.com>
2021-07-07 08:48:17 -0400

    Add new packages and home page grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import "./Routing";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">The MacMurray's are Awesome!</header>
+      <main>
+        <div class="grid">
+          <div class="cols-5">
+            <div class="inner">
+              <h3>
+                <a href="https://createadrln.github.io/macmurray-chores/chores">Accomplishments</a>
+              </h3>
+              <p>Accomplishments</p>
+            </div>
+          </div>
+          <div class="cols-5">
+            <div class="inner">
+              <h3>
+                <a href="https://createadrln.github.io/macmurray-chores/chores">Chores</a>
+              </h3>
+              <p>Randomly assign chores</p>
+            </div>
+          </div>
+          <div class="cols-5">
+            <div class="inner">
+              <h3>
+                <a href="https://createadrln.github.io/macmurray-chores/chores">Calendars</a>
+              </h3>
+              <p>View our calendars</p>
+            </div>
+          </div>
+          <div class="cols-5">
+            <div class="inner">
+              <h3>
+                <a href="https://createadrln.github.io/macmurray-chores/chores">Activities</a>
+              </h3>
+              <p>Find things to do</p>
+            </div>
+          </div>
+          <div class="cols-5">
+            <div class="inner">
+              <h3>
+                <a href="https://createadrln.github.io/macmurray-chores/chores">Recipes</a>
+              </h3>
+              <p>Our favorite recipes.</p>
+            </div>
+          </div>
+        </div>
+        <div class="grid">
+          <div class="cols-5">
+            <div class="inner">
+              <h3>
+                <a href="https://createadrln.github.io/macmurray-chores/chores">Rachael</a>
+              </h3>
+              <p>Social Links</p>
+            </div>
+          </div>
+          <div class="cols-5">
+            <div class="inner">
+              <h3>
+                <a href="https://createadrln.github.io/macmurray-chores/chores">Ryan</a>
+              </h3>
+              <p>Social Links</p>
+            </div>
+          </div>
+          <div class="cols-5">
+            <div class="inner">
+              <h3>
+                <a href="https://createadrln.github.io/macmurray-chores/chores">Tyler</a>
+              </h3>
+              <p>Social Links</p>
+            </div>
+          </div>
+          <div class="cols-5">
+            <div class="inner">
+              <h3>
+                <a href="https://createadrln.github.io/macmurray-chores/chores">Audrey</a>
+              </h3>
+              <p>Social Links</p>
+            </div>
+          </div>
+          <div class="cols-5">
+            <div class="inner">
+              <h3>
+                <a href="https://createadrln.github.io/macmurray-chores/chores">Jojo and Crowley</a>
+              </h3>
+              <p>Social Links</p>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
